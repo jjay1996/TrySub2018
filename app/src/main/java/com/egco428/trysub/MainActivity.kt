445fa534ca9a37b506c93c312085cc67990b20e9
Kanpruchong Sripetch <kanpruchong.sri@student.mahidol.edu>
2018-11-02 13:30:01 +0700

    1st commit

---
+++ app/src/main/java/com/egco428/trysub/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.egco428.trysub
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/egco428/trysub/PlayActivity.kt
@@ -0,0 +1,12 @@
+package com.egco428.trysub
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play)
+    }
+}
