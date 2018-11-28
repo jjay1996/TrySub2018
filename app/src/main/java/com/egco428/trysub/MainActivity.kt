b164c442d99060c570dd7028d514e98fdc1f16ca
Kanpruchong Sripetch <kanpruchong.sri@student.mahidol.edu>
2018-11-28 19:17:51 +0700

    test DB

--- app/src/main/java/com/egco428/trysub/MainActivity.kt
+++ app/src/main/java/com/egco428/trysub/MainActivity.kt
@@ -8,7 +8,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var objects:QuestionAnswer = QuestionAnswer()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/egco428/trysub/QuestionAnswerActivity.kt
+++ app/src/main/java/com/egco428/trysub/QuestionAnswerActivity.kt
@@ -2,8 +2,11 @@ package com.egco428.trysub
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class QuestionAnswerActivity : AppCompatActivity() {
+
+    var objects:QuestionAnswer = QuestionAnswer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
